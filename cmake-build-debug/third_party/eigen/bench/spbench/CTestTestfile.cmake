0787cd1d54201dbcbe74ecfb992ad8f86871a036
KirillGavrish <k_gavrish5413@mail.ru>
2024-10-26 02:02:26 +0300

    first morm commit

--- cmake-build-debug/third_party/eigen/bench/spbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bennington2000super/Documents/5 семестр/Вычматы/labs/third_party/eigen/bench/spbench
-# Build directory: /Users/bennington2000super/Documents/5 семестр/Вычматы/labs/cmake-build-debug/third_party/eigen/bench/spbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/third_party/eigen/unsupported/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bennington2000super/Documents/5 семестр/Вычматы/labs/third_party/eigen/unsupported
-# Build directory: /Users/bennington2000super/Documents/5 семестр/Вычматы/labs/cmake-build-debug/third_party/eigen/unsupported
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Eigen")
-subdirs("test")
